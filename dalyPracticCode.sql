64cd2d37c56c033aaf42e3d5444b39d49758f89d
Srimanta Pal <srimantapal205@hotmail.com>
2024-06-07 23:47:30 +0530

    DISTINCT challange

--- dalyPracticCode.sql
+++ dalyPracticCode.sql
@@ -3,5 +3,21 @@ GO
 
 SELECT * FROM hcm.employees;
 
+--First challange Solution
 SELECT employee_id, first_name, last_name, salary FROM hcm.employees;
 SELECT employee_id as ID, first_name+' '+last_name as Name, salary from hcm.employees;
+
+--Second challange Solution
+SELECT last_name as customer_last_name, city FROM hcm.employees; 
+
+--Third challange Solution
+SELECT * FROM oes.order_items;
+
+--Selecting all column from the table.
+SELECT * FROM bird.antarctic_populations;
+
+--First  DISTINCT challange solution 
+SELECT DISTINCT locality FROM bird.antarctic_populations;
+
+--Second  DISTINCT challange solution 
+SELECT DISTINCT locality, species_id FROM bird.antarctic_populations;
