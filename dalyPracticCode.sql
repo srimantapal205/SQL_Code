6603d9170809c354f8cbbe01b83b4161a17b3518
Srimanta Pal <srimantapal205@hotmail.com>
2024-06-08 23:43:07 +0530

    -- isnull function

--- dalyPracticCode.sql
+++ dalyPracticCode.sql
@@ -34,9 +34,21 @@ SELECT * from hcm.employees ORDER BY salary DESC;
 SELECT employee_id as ID, first_name+' '+last_name as Name, hire_date from hcm.employees ORDER BY hire_date DESC;
 SELECT * from hcm.employees ORDER BY hire_date DESC;
 
-SELECT employee_id as ID, department_id as Dept_ID , first_name+' '+last_name as Name, salary from hcm.employees ORDER BY Dept_ID ASC , salary DESC ;
+SELECT employee_id as ID, department_id as Dept_ID , first_name+' '+last_name as Name, salary from hcm.employees where department_id IS  NOT NULL ORDER BY Dept_ID ASC , salary DESC;
 SELECT * from hcm.employees ORDER BY department_id ASC , salary DESC ;
 
 SELECT TOP (10) employee_id as ID, first_name, last_name, salary FROM hcm.employees ORDER BY salary DESC ;
 
 SELECT  TOP (1) WITH TIES employee_id as ID, first_name, last_name, salary FROM hcm.employees ORDER BY salary ASC ;
+
+-- isnull function 
+-- ISNULL(<field-name>,0 or Deafult value)
+-- COALESCE(expression-1, expression-1, expression-2, ..... expression-n) We can specifi more than two expression when using the coalesc function and the function will return the  value of the first non null expression of each row.
+ 
+
+
+
+
+-- NULL
+
+
