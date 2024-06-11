5ee04124ba8c3a28077efe6bd1f1183ffab42324
Srimanta Pal <srimantapal205@hotmail.com>
2024-06-11 11:32:10 +0530

    -- GROUP BY Challenges

--- dalyPracticCode.sql
+++ dalyPracticCode.sql
@@ -84,4 +84,24 @@ SELECT * FROM oes.products WHERE product_name LIKE '%mouse%';
 --Challenge- 4: Select all product names from the oes.products table which end in a number.
 SELECT * FROM oes.products WHERE product_name LIKE '%[0-9]';
 
+-- GROUP BY Challenges
 
+SELECT * FROM hcm.employees;
+-- Challenge-1: Write query to give the total number of employees in each department as given by the department_id column in the hcm.employees table.
+SELECT COUNT(employee_id ) AS 'Number Of Employee'  FROM hcm.employees;
+SELECT  DISTINCT department_id , COUNT(employee_id ) AS 'Number Of Employee'  FROM hcm.employees GROUP BY  department_id ;
+
+-- Challenge-2:Write a query to give the average salary in each  department as given by the department_id column in the hcm.employees table. Order the query result by average salary from highest to lowest.
+SELECT  department_id, AVG(salary) AS 'AverageSalary' FROM hcm.employees  GROUP BY  department_id ORDER BY  AverageSalary DESC ;
+
+SELECT * FROM oes.inventories;
+--Challenge-3: Write a query to give the total number of products on hand at each warehouse as given by the warehouse_id column in the oes.inventories table. Also, limit the result to only warehouses which have greater than 5,000 product items on hand
+SELECT warehouse_id, SUM(quantity_on_hand) AS 'TotalItem' FROM oes.inventories  GROUP BY warehouse_id HAVING SUM(quantity_on_hand) >= 5000;
+
+SELECT * FROM bird.antarctic_populations;
+--Challenge-4: What is the date of the most recent population count at each locality in the bird.antarctic_populations table
+SELECT locality, MAX([date_of_count]) AS  most_recent_population_count FROM bird.antarctic_populations GROUP BY locality;
+
+--Challenge-5: What is the date of the most recent population count for each species at each locality in the bird.antarctic_populations table
+
+SELECT species_id, locality, MAX([date_of_count]) AS  most_recent_population_count FROM bird.antarctic_populations GROUP BY species_id,locality;
