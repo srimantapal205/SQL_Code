131df3c4152598bd781475085a0b3baf2a0711bc
Srimanta Pal <srimantapal205@hotmail.com>
2024-06-12 23:28:28 +0530

    --Logical Operator Challenges

--- Section1-SQLStatementFundamentals.sql
+++ Section1-SQLStatementFundamentals.sql
@@ -106,3 +106,19 @@ SELECT locality, MAX([date_of_count]) AS  most_recent_population_count FROM bird
 
 SELECT species_id, locality, MAX([date_of_count]) AS  most_recent_population_count FROM bird.antarctic_populations GROUP BY species_id,locality;
 
+ --Logical Operator Challenges
+
+ --Challenge 1:Select employees from the hcm.employees table who live in either Seattle or Sydney.
+  SELECT * FROM hcm.employees WHERE city = 'Seattle' OR city = 'Sydney';
+ --Challenge 2:Select employees who live in any of the following cities:-Seattle-Sydney-Ascot-Hillston:
+ SELECT * FROM hcm.employees WHERE city IN ('Seattle' ,'Sydney', 'Ascot', 'Hillston');
+
+ --Challenge 3:Select employees from Sydney who have a salary greater than $200,000.
+ SELECT * FROM hcm.employees WHERE city = 'Sydney' AND salary > 200000
+ --Challenge 4:Select employees who live in either Seattle or Sydney and were also hired on or after 1 st January 2019.
+  SELECT * FROM hcm.employees WHERE (city = 'Seattle' OR city = 'Sydney') AND hire_date >= '20190101';
+ --Challenge 5:Select products from the oes.products table which do not have a product category_id of either 1, 2, or 5.
+ SELECT * FROM  oes.products WHERE category_id NOT IN (1,2,5);
+
+
+
