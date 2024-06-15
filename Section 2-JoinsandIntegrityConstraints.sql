654625ae2e16813a1d8177746a368b2fea85aee7
Srimanta Pal <srimantapal205@hotmail.com>
2024-06-15 07:51:29 +0530

    Join with --Challenge

--- Section 2-JoinsandIntegrityConstraints.sql
+++ Section 2-JoinsandIntegrityConstraints.sql
@@ -1,9 +1,11 @@
 USE SAMPLEDB
 GO
 
+SELECT  * FROM hcm.employees;
+SELECT COUNT(*) as total_emp, e.department_id from hcm.employees AS e GROUP BY e.department_id;
 --Join
 
-
+SELECT  * FROM hcm.departments AS d  JOIN hcm.employees AS e ON d.department_id = e.department_id;
 --Inner join
 --SELECT FROM departments INNER JOIN employees ON departments dept_id employees dept_id
 
@@ -16,9 +18,12 @@ GO
 --last_name
 --salary
 --department_name
-SELECT  e.first_name, e.last_name, e.salary,d.department_name FROM hcm.departments AS d INNER JOIN hcm.employees AS e ON d.department_id = e.department_id;
-SELECT  e.first_name, e.last_name, e.salary,d.department_name FROM hcm.departments AS d LEFT OUTER JOIN hcm.employees AS e ON d.department_id = e.department_id;
+SELECT e.employee_id, e.first_name, e.last_name, e.salary, d.department_name FROM hcm.employees AS e INNER JOIN hcm.departments AS d ON e.department_id = d.department_id;
 
+
+
+--RIGHT OUTER JOIN
+SELECT  e.first_name, e.last_name, e.salary,d.department_name FROM hcm.departments AS d  INNER JOIN hcm.employees AS e ON d.department_id = e.department_id;
 
 --Challenge-2:Write a query to return the following attributes for all employees, including employees who do not belong to a department:
 --employee_id
@@ -27,6 +32,11 @@ SELECT  e.first_name, e.last_name, e.salary,d.department_name FROM hcm.departmen
 --salary
 --department_name
 
-
-
+SELECT e.employee_id, e.first_name, e.last_name, e.salary,d.department_name FROM hcm.employees AS e LEFT OUTER JOIN hcm.departments AS d ON d.department_id = e.department_id WHERE d.department_name IS NULL;
+SELECT e.employee_id, e.first_name, e.last_name, e.salary,d.department_name FROM hcm.departments AS d RIGHT OUTER JOIN hcm.employees AS e ON d.department_id = e.department_id
 --Challenge-3:Write a query to return the total number of employees in each department. Include the department_name in the query result. Also, include employees who have not been assigned to a department.
+SELECT * FROM hcm.departments;
+SELECT COUNT(*) as total_emp_in_dep, e.department_id from hcm.employees AS e GROUP BY e.department_id;
+SELECT d.department_name, d.department_id, COUNT(e.employee_id) AS total_no_employee  FROM hcm.departments AS d  LEFT JOIN hcm.employees AS e ON d.department_id = e.department_id GROUP BY d.department_name, d.department_id;
+SELECT d.department_name,  d.department_id,  COUNT(*) AS total_no_employee  FROM hcm.employees e LEFT JOIN hcm.departments AS d ON d.department_id = e.department_id GROUP BY d.department_name,  d.department_id;
+
