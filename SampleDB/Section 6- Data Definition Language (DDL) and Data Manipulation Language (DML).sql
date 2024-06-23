e8c318514da39a41cdc76a1f1f44e7e69cb9c3ee
Srimanta Pal <srimantapal205@hotmail.com>
2024-06-23 16:34:04 +0530

    Create  example table

--- SampleDB/Section 6- Data Definition Language (DDL) and Data Manipulation Language (DML).sql
+++ SampleDB/Section 6- Data Definition Language (DDL) and Data Manipulation Language (DML).sql
@@ -71,6 +71,91 @@ END
 When  using float data type the value 10 can be stored accurectly. How ever the  value 0.1 cannot be accuratly represented in float . There fore , event though we had the WHILE condition @val !=10.0 the @val will never exactly equal 10 when we use the FLOAT data type. this means that the loop will keep interating - by adding 0.1 - indefinitely.
 
 However, if we look in the message tab we can see that it does show that at one point there was a value of 10. but  this is just SQL server management Studion is rounding the values that are printed. In other word , SSMS is not showing us the actual values.
+To get a more accurate picture of the float value we can use the STR function to convert the float to character string::
+
+*/
+DECLARE @val FLOAT(24) = 0.0
+WHILE @val != 10.0
+BEGIN
+	PRINT str(@val,20,16 ); -- STR (float_expression [, length[, decial]])
+	SET @VAL += 0.1;
+END;
+
+
+/*
+CREATE TABLE::
+SYNTAX ::
+CREATE TABLE table_name
+(
+	column-1 data-type [NOT NULL],
+	column-2 data-type ,
+	CONSTRAINT constraint_name constraint_type (column 1)
+)
+
+Example::
+CREATE TABLE departments
+(
+	department_id INT IDENTITY(1,1)
+	department_name VARCHAR(50) NOT NULL
+	CONSTRAINT PK_department_id PRIMARY KEY (department_id)
+
+)
 
 */
 
+--Create table parks-1
+CREATE TABLE parks1
+(
+	park_id INT,
+	park_name VARCHAR(50),
+	entry_fee DECIMAL(6,2)
+)
+
+SELECT * FROM dbo.parks1;
+
+---Insert data in the table.
+INSERT INTO dbo.parks1 (park_id, park_name,entry_fee)
+	VALUES(1, 'Bellmont Park', 5)
+
+INSERT INTO dbo.parks1 (park_id, park_name,entry_fee)
+	VALUES(2, 'Redmond Park', 10)
+
+INSERT INTO dbo.parks1 (park_id, park_name,entry_fee)
+	VALUES(3, 'Highland Mountains', 5.74)
+
+--Create table parks-2
+CREATE TABLE parks2
+(
+	park_id INT IDENTITY(1,1),
+	park_name VARCHAR(50),
+	entry_fee DECIMAL(6,2),
+	CONSTRAINT PK_parks2_park_id PRIMARY KEY(park_id)
+)
+
+SELECT * FROM dbo.parks2;
+
+---Insert data in the table.
+INSERT INTO dbo.parks2 (park_name,entry_fee)
+	VALUES( 'Bellmont Park', 5)
+
+INSERT INTO dbo.parks2 (park_name,entry_fee)
+	VALUES('Redmond Park', 10)
+
+INSERT INTO dbo.parks2 (park_name,entry_fee)
+	VALUES('Highland Mountains', 5.74)
+
+
+--Create child table called park_visits::
+
+CREATE TABLE dbo.park_visits
+	(
+		visit_id INT IDENTITY,
+		park_id INT NOT NULL,
+		visit_date DATE NOT NULL,
+		first_name NVARCHAR(50) NOT NULL,
+		last_name NVARCHAR(50) NOT NULL
+		CONSTRAINT PK_park_visits_visit_id PRIMARY KEY(visit_id),
+		CONSTRAINT FK_park_visitis_id FOREIGN KEY (park_id) REFERENCES parks2 (park_id)
+	);
+
+SELECT * FROM dbo.park_visits;
