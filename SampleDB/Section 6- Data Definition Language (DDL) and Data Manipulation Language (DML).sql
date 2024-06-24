90339e74520fcba6391d651b9b9af4186ac417df
Srimanta Pal <srimantapal205@hotmail.com>
2024-06-24 23:11:41 +0530

    SQL UPDATE Statement

--- SampleDB/Section 6- Data Definition Language (DDL) and Data Manipulation Language (DML).sql
+++ SampleDB/Section 6- Data Definition Language (DDL) and Data Manipulation Language (DML).sql
@@ -361,3 +361,37 @@ INSERT INTO dbo.subjects3 (subject_name)
 			('English')
 
 SELECT * FROM dbo.subjects3;
+
+
+/*
+
+UPDATE Statement
+
+The UPDATE statement is used to update exiting data in a table.
+Syntax ::
+UPDATE table_name
+SET column1 = value1,
+	column2 = value2,
+	----
+	----
+WHERE <Conditions>;
+
+
+Example::
+UPDATE products SET price = 5.5 WHERE product_name = 'Sports hat';
+
+--Increase the price on particuler product
+Example 2 ::
+UPDATE products SET price = price * 1.1 WHERE product_name = 'Sports hat';
+UPDATE products 
+	SET 
+		price = price * 1.1 
+		category_name = 'Clothing';
+WHERE product_name = 'Sports hat';
+*/
+SELECT * FROM dbo.emp;
+
+UPDATE dbo.emp
+	SET last_name = 'Greenbank' 
+WHERE emp_id = 2
+
